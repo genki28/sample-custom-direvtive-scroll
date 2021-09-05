7fe52e4373b1bf0b031c81bd39f0d8dc5c8785fd
GenkiOsawa <taiiku39@gmail.com>
2021-09-06 02:22:16 +0900

    custom directive

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,40 +1,47 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div v-for="animal in animals" :key="animal.id">ID:{{animal.id}} name:{{animal.name}}</div>
+    <div v-animal-scroll="infiniteHandler"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Main',
-  props: {
-    msg: String
+  data() {
+    return {
+      animals: [],
+      animalData: [],
+      start: 0,
+      end: 40,
+      // データ更新を行うスクロール量
+      startScrollYOffset: 0
+    }
+  },
+  mounted() {
+    const list = ['サーバル', 'フェネック', 'アライグマ', 'カバ', 'コツメカワウソ', 'ジャガー', 'トキ']
+    for(let i = 0; i < 200; i++) {
+      this.animalData.push({id: i, name: list[Math.floor(Math.random()*list.length)]})
+    }
+    this.startScrollYOffset = Math.floor(window.innerHeight / 3)
+    this.getAnimals()
+  },
+  methods: {
+    infiniteHandler() {
+      // 現時点でのスクロール量が取得を行うスクロール量以上
+      if (window.pageYOffset >= this.startScrollYOffset) {
+          this.startScrollYOffset = window.innerHeight + window.pageYOffset
+          this.getAnimals()
+      }
+    },
+    // axiosとかのAPIの想定
+    getAnimals() {
+      if (this.end <= this.animalData.length) {
+        this.animals = this.animals.concat(this.animalData.slice(this.start, this.end))
+        this.start = this.start + 40
+        this.end = this.end + 40
+      }
+    }
   }
 }
 </script>
